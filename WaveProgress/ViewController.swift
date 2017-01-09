39938078602cc1a20fb2e0010d5b428944fab9be
许绍松 <xss@ttyhuo.cn>
2017-01-09 10:38:14 +0800

    Initial Commit

---
+++ WaveProgress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaveProgress
+//
+//  Created by xss@ttyhuo.cn on 2017/1/9.
+//  Copyright © 2017年 xss@ttyhuo.cn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WaveProgressTests/WaveProgressTests.swift
@@ -0,0 +1,36 @@
+//
+//  WaveProgressTests.swift
+//  WaveProgressTests
+//
+//  Created by xss@ttyhuo.cn on 2017/1/9.
+//  Copyright © 2017年 xss@ttyhuo.cn. All rights reserved.
+//
+
+import XCTest
+@testable import WaveProgress
+
+class WaveProgressTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
